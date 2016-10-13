d84c9460768289e59b31cabef2c25057793ce9a1
Jean-Rémy Falleri <jr.falleri@gmail.com>
2016-10-13 21:52:28 +0200

    fix lint error.

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * Index.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   Hello
+ * @author    Jean-Rémy Falleri <jr.falleri@gmail.com>
+ * @copyright 2016 Jean-Rémy Falleri
+ * @license   BSD License
+ * @link      https://github.com/jrfaller/php-stack-test
+ */
+
 require_once 'hello.php';
 
 $hello = new Hello('JRe');
