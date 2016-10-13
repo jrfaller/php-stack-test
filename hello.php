e49bd2d460022ed5c1a9f414fdf855944b81f55d
Jean-Remy Falleri <jr.falleri@gmail.com>
2016-10-13 12:41:30 +0000

    make use of twig and phing.

--- hello.php
+++ hello.php
@@ -1,13 +1,23 @@
 <?php
 
+require_once 'vendor/autoload.php';
+Twig_Autoloader::register();
+
 class Hello {
     var $name;
+    var $twig;
 
     function say_hello() {
         return "Hello " . $this->name . "!";
     }
 
+    function hello_page() {
+        $template = $this->twig->loadTemplate('hello.tpl');
+        return $template->render(array('hello' => $this->say_hello()));
+    }
+
     function Hello($name) {
         $this->name = $name;
+        $this->twig = new Twig_Environment(new Twig_Loader_Filesystem('.'), array('cache' => '.'));
     }
 }
--- test_hello.php
+++ test_hello.php
@@ -10,5 +10,12 @@ class HelloTest extends PHPUnit_Framework_TestCase
         $h = new Hello('JRe');
         $this->assertEquals("Hello JRe!", $h->say_hello());
     }
+
+    public function testHelloPage() {
+        $h = new Hello('JRe');
+        $p = $h->hello_page();
+        $this->assertContains('Hello JRe!', $p);
+        $this->assertContains('<html>', $p);
+    }
 }
 
